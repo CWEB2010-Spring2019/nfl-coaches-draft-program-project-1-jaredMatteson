7a94758dd18946feeb5186d103b26708987da247
Matteson <matjara@dunwoody.edu>
2019-02-10 16:23:44 -0600

    Redoing file path and how i read in json alomg with players class to get all info neccesary

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,21 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.IO;
+using Newtonsoft.Json;
 
 namespace project1
 {
-    class Program
+     class Program
     {
-       
+       static List<Players> players;
+
+        public static void LoadJson()
+        {
+            using (StreamReader r = new StreamReader("players.json"))
+            {
+                string json = r.ReadToEnd();
+                players = JsonConvert.DeserializeObject<List<Players>>(json);
+            }
+        }
     }
+
+    
+
     class Players
     {
-        private string name { get; set; }
-        private string school { get; set; }
-        private int salary { get; set; }
-        private int rank { get; set;  }
-        private string position { get; set; }
+        private string Name { get; set; }
+        private string School { get; set; }
+        private int Salary { get; set; }
+        private int Rank { get; set;  }
+        private string Position { get; set; }
 
+        /*
         private Players(string name, string school, int salary, int rank, string position )
         {
             this.name = name;
@@ -23,9 +37,8 @@ namespace project1
             this.salary = salary;
             this.rank = rank;
             this.position = position;
-
         }
-        
+        */
         
     }
 }
